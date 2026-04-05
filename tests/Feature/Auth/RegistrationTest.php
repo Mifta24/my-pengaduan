3ef1b806b402e459ddc69897c46d2c687abce37f
Miftah <miftafree3@gmail.com>
2026-04-05 13:47:56 +0700

    Update registration test required payload

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Auth;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
@@ -21,8 +22,15 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'nik' => '1234567890123456',
+            'ktp_file' => UploadedFile::fake()->image('ktp.jpg'),
+            'address' => 'Alamat Test',
+            'phone' => '081234567890',
+            'rt_number' => '001',
+            'rw_number' => '002',
             'password' => 'password',
             'password_confirmation' => 'password',
+            'terms' => true,
         ]);
 
         $this->assertAuthenticated();
